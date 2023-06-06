549c92791af2ab8c7e3673dd0b29e654be8c8c62
Valeria48562 <vblagodatskaja@gmail.com>
2023-06-06 16:45:04 +0300

    main

--- etch.js
+++ etch.js
@@ -1,10 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const container = document.querySelector("#grid-container");
+const container = document.querySelector(".grid-container");
 
-  for (let i = 0; i < 16; i++) {
-    for (let j = 0; j > 16; j++) {
-      const div = document.createElement("div");
-      container.appendChild(div);
-    }
+for (let i = 0; i < 16; i++) {
+  for (let j = 0; j < 16; j++) {
+    const div = document.createElement("div");
+    container.appendChild(div);
   }
-});
+}
